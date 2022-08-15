751f11e76e766991495eabb67144d9782db48e74
anjandash <anjan8@gmail.com>
2022-08-15 20:39:28 +0200

    changes

--- finetune/exp/evaluatex.py
+++ finetune/exp/evaluatex.py
@@ -1,28 +1,22 @@
-from transformers import AutoTokenizer, TrainingArguments, Trainer
+from transformers import AutoTokenizer
 from transformers import AutoModelForMaskedLM
-from datasets import Dataset
-from datasets import ClassLabel
-from datasets import load_dataset
 from sklearn.model_selection import train_test_split
-from transformers import AutoModelForMaskedLM
-from transformers import DataCollatorForLanguageModeling
 from transformers import pipeline
 
 import pandas as pd
 import numpy as np
 import sys
 
-epochs = 10
-block_size = 512
+
 model_checkpoints = ["bert-base-uncased", "huggingface/CodeBERTa-small-v1", "microsoft/codebert-base-mlm", "microsoft/graphcodebert-base"]
 
 model_checkpoint = model_checkpoints[1]
-model_checkpoint_path =  "/home/akarmakar/codekitty/finetune/exp/CodeBERTa-small-v1-finetuned-jemma_COMP_MAIN_CSPACE/checkpoint-20000"
 model_dataset = "jemma_COMP_MAIN_CSPACE"
+checkpoint_number = 20000
 
+model_checkpoint_path =  "/home/akarmakar/codekitty/finetune/exp/"+model_checkpoint+"-finetuned-"+model_dataset+"/checkpoint-"+checkpoint_number
 train_csv_path = "/home/akarmakar/codekitty/data/"+model_dataset+"/JEMMA_COMP_train_MAIN.csv"
 valid_csv_path  = "/home/akarmakar/codekitty/data/"+model_dataset+"/JEMMA_COMP_valid_MAIN.csv"
-datasets = load_dataset("csv", data_files={"train": train_csv_path, "test": valid_csv_path})
 
 print("Going for prediction ... ")
 print("_________________________")
@@ -33,13 +27,11 @@ test_data = pd.read_csv(test_csv_path, header=0) #load_dataset(dataset_name, spl
 X_test = list(test_data["text"])
 y_test = list(test_data["labels"])
 
-
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 model = AutoModelForMaskedLM.from_pretrained(model_checkpoint_path)
 predictor = pipeline(task="fill-mask", model=model, tokenizer=tokenizer)
 
+orig = []
 pred = []
 for snippet, orig_label in zip(X_test, y_test):
     snippet = snippet.replace("[MASK]", "<mask>")
@@ -51,4 +43,10 @@ for snippet, orig_label in zip(X_test, y_test):
 
     print("orig_label:", orig_label)
     print("pred_label:", pred_label)
-    break
+
+    orig.append(orig_label)
+    pred.append(pred_label)
+
+# pred_dict = {"orig": orig, "pred": pred_label} 
+# df = pd.DataFrame([pred_dict])
+# df.to_csv(f"{model_checkpoint_path}/eval_pred.csv", index=False)
